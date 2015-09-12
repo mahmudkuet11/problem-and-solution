fe080e189dea90988a9cfbf972056b3d02c6052e
Mahmud Raju <mahmudkuet11@gmail.com>
2015-09-12 14:43:36 +0600

    image in jasper report

--- php/laravel_file_upload.php
+++ php/laravel_file_upload.php
@@ -4,7 +4,7 @@
 		// If the uploads fail due to file system, you can try doing public_path().'/uploads' 
 		$filename = $file->getClientOriginalName();
 		$extension =$file->getClientOriginalExtension(); 
-		$upload_success = Input::file('file')->move($destinationPath, $filename.$extension);
+		$upload_success = Input::file('file')->move($destinationPath, $filename);
 
 		if( $upload_success ) {
 		   return 'ok';
